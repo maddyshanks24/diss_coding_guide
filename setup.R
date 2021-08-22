9293e8ed2913caa637ab2af0ee41aca6b0eaf3eb
Maddy Shanks <maddyshanks@macbook-pro.lan>
2021-08-22 14:37:37 -0400

    initial commit

---
+++ setup.R
@@ -0,0 +1,15 @@
+
+## start up - install package
+remotes::install_github("juba/rmdformats")
+
+## check wd
+getwd()
+
+## install distill
+remotes::install_github("rstudio/distill")
+
+install.packages("distill")
+
+## create site shell
+## enter name when prompted
+distill::create_website(dir = getwd(), gh_pages = TRUE)
