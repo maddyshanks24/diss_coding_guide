79ad9fd0a05e49feefdd4419ecff1a21e40949e9
Maddy Shanks <maddyshanks@macbook-pro.lan>
2021-08-23 12:36:34 -0400

    includes information on all pages using rmdformats and robobook

--- setup.R
+++ setup.R
@@ -18,3 +18,14 @@ remotes::install_github("juba/rmdformats")
 ## TASKS
 ## to render site
 rmarkdown::render_site()
+
+## to convert latex to markdown
+rmarkdown::pandoc_convert(input = "../../../Prospectus/Grants/DDRIG/Data Management Plan.tex",
+                          to = "markdown",
+                          from = "latex",
+                          citeproc = TRUE)
+
+## in terminal using pandoc
+## navigate to wd using cd (set the wd) and ls (list things to check which wd)
+## use pandoc code below to get citations
+##pandoc -t markdown-citations -s "Project Description.tex" -o "projectdescription.md" --bibliography "prospectus4.bib"
